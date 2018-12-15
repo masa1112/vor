d08a295886c49c0cab4d30320d283c3394b34e4d
Masazumi Okada <1514020@ed.tus.ac.jp>
2018-12-15 17:10:32 +0900

    done

--- test.py
+++ test.py
@@ -1,27 +1,29 @@
 import pandas as pd
 import csv
 import matplotlib
-
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d
+from PIL import Image
+images = []
+p = 0
+for l in open('vor.csv').readlines():
+    #data = l[:-1].split(',')
+    data = [float(y.strip()) for y in l.split(',')]
+    x = []
 
-x=[[]]
-for l in open('offw.csv').readlines():
-    data = l[:-1].split(',')
-#data = pd.read_csv('offw.csv', header=None, delim_whitespace=True, decimal=',')
-#data = np.genfromtxt("offwb.txt", dtype = None, delimiter = "¥t")
-for i in range(0,30):
-    for j in range(0,100):
-        #x += [[float(data[i][j*2]),float(data[i][j*2+1])]]
-
-        #x[i][j] += [[data[i][j*2],data[i][j*2+1]]]
-        x[j] += [[data[j * 2], data[j * 2 + 1]]]
+    for j in range(0,int(len(data)/2)):
+        a = j * 2
+        b = a + 1
+        x.append([float(data[a]), float(data[b])])
     points = x
     vor = Voronoi(points)
     plt.figure(figsize=(6, 4), facecolor='white')
-    ax = plt.subplot(aspect='equal')
     voronoi_plot_2d(vor)
-#plt.show()
-    plt.savefig('data/figure.png')
+
+    plt.savefig('data/fig_{0}.png'.format(p))
+    images = [Image.open('data/fig_{0}.png'.format(p))]
+    p += 1
+images[0].save('data/out.gif', save_all=True, append_images=images[1:], duration=1000, loop=0)
+plt.show()
