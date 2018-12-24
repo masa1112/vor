b51292026b00db738cdb48caf15d8fa7a1c0d35a
Sumino Lab <>
2018-12-24 18:59:02 +0900

    no message

--- test.py
+++ test.py
@@ -4,10 +4,14 @@ import matplotlib
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.spatial import Voronoi, voronoi_plot_2d
+from scipy.spatial import Voronoi, voronoi_plot_2d, Delaunay
+from collections import defaultdict
+import itertools
+
 from PIL import Image
 images = []
 p = 0
+s = 0
 for l in open('vor.csv').readlines():
     #data = l[:-1].split(',')
     data = [float(y.strip()) for y in l.split(',')]
@@ -18,14 +22,27 @@ for l in open('vor.csv').readlines():
         b = a + 1
         x.append([float(data[a]), float(data[b])])
     points = x
+    tri = Delaunay(points)
+    neiList = defaultdict(set)
+    for q in tri.vertices:
+        for e, r in itertools.combinations(q, 2):
+            neiList[e].add(r)
+            neiList[r].add(e)
+    print("fig ( %d )" % (s))
+    s += 1
+    for key in sorted(neiList.keys()):
+        print("%d:%s" % (key, ','.join([str(h) for h in neiList[key]])))
     vor = Voronoi(points)
-    plt.xlim(0,1000)
-    plt.ylim(0,40)
+    #plt.xlim(0,1000)
+    #plt.ylim(0,40)
     plt.figure(figsize=(6, 4), facecolor='white')
     voronoi_plot_2d(vor)
+    #vr = vor.region
+    for i, c in enumerate(x):
+        plt.text(c[0], c[1], '#%d' % i, ha='center')
 
-    #plt.savefig('data/fig_{0}.png'.format(p))
+    plt.savefig('data/fig_{0}.png'.format(p))
     images.append(Image.open('data/fig_{0}.png'.format(p)))
     p += 1
-images[0].save('data/out.gif', save_all=True, append_images=images[1:], duration=400, loop=0)
+images[0].save('data/out.gif', save_all=True, append_images=images[1:], duration=100, loop=0)
 plt.show()
